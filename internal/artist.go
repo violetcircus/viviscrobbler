21352efe0143f7ad7e6ff857255749a9af81e315
violet <iaresmarts@gmail.com>
2025-05-27 03:20:19 +0100

    hi

--- internal/artist.go
+++ internal/artist.go
@@ -11,7 +11,7 @@ func GetArtist(track_info map[string]string) string {
 	log.SetFlags(0)
 
 	//check if artist field starts with album artist, overwrite if it does
-	//this takes care of edge cases that can't be solved with regex.
+	//this takes care of SOME edge cases that can't be solved with regex.
 	if strings.HasPrefix(artist, album_artist) {
 		artist = album_artist
 	} else {
@@ -23,20 +23,19 @@ func GetArtist(track_info map[string]string) string {
 func SeparateArtists(artist string) string {
 	// list of artist separators to check for. could get from config file.
 	//replace with regex??
-	separators := []string {
-		","," x ",";","/","&","feat.","Featuring","featuring",
+	separators := []string{
+		"feat.","Featuring","featuring"," x ",",",";","/","&",
 	}
 	// slice containing attempts to find 1st artist name
 	attempts := []string{}
 
 	// loop over the separators checking if any show up in the artist string
 	for _, separator := range separators {
-		artist, _, found := strings.Cut(artist, separator)
+		artist, found := strings.CutPrefix(artist, separator)
 		if found {
 			attempts = append(attempts, artist)
 			// log.Print(separator)
 			// log.Print("artist", artist)
-			// log.Print("features", features)
 		}
 		// log.Print("no separator found!!")
 	}
